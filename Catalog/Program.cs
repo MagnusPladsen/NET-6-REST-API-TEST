ebe3f8bcef9b83ba6653c908a5ce38fe56b34ef1
Magnus Pladsen <93226629+MagnusPladsen@users.noreply.github.com>
2022-11-29 16:17:28 +0100

    Started on WEBAPI

---
+++ Catalog/Program.cs
@@ -0,0 +1,46 @@
+using Catalog.Repositories;
+using Catalog.Settings;
+using MongoDB.Bson;
+using MongoDB.Bson.Serialization;
+using MongoDB.Bson.Serialization.Serializers;
+using MongoDB.Driver;
+using System.Configuration;
+
+var builder = WebApplication.CreateBuilder(args);
+
+// Add services to the container.
+
+builder.Services.AddControllers();
+/* builder.Services.AddSingleton<IMongoClient>(serviceProvider => 
+    {
+        var settings = Configuration.GetSection(nameof(MongoDbSettings)).Get<MongoDbSettings>();
+        return new MongoClient(settings.ConnectionString);
+    }); */
+
+// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen();
+builder.Services.AddSingleton<IItemsRepository, MongoDbItemsRepository>();
+
+
+BsonSerializer.RegisterSerializer(new GuidSerializer(BsonType.String));
+BsonSerializer.RegisterSerializer(new DateTimeOffsetSerializer(BsonType.String));
+builder.Configuration.Bind(nameof(MongoDbSettings), builder.Services.AddOptions<MongoDbSettings>());
+
+
+var app = builder.Build();
+
+// Configure the HTTP request pipeline.
+if (app.Environment.IsDevelopment())
+{
+    app.UseSwagger();
+    app.UseSwaggerUI();
+}
+
+app.UseHttpsRedirection();
+
+app.UseAuthorization();
+
+app.MapControllers();
+
+app.Run();
